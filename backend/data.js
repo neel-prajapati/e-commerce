0ecb4cda681f9502caead33713f56f64696025b0
neel-prajapati <45667399+neel-prajapati@users.noreply.github.com>
2020-09-24 12:29:37 +0530

    Initial commit of mobile shopping web-app

---
+++ backend/data.js
@@ -0,0 +1,64 @@
+export default
+{
+    products:[
+    {
+        _id:'1',
+        name: 'OnePlus 8',
+        image:'../images/oneplus.jpg',
+        price: '₹49,500',
+        brand: 'OnePlus',
+        rating: 4.2,
+        numReviews: 11
+
+    },
+    {
+        _id:'2',
+        name: 'iPhone 11',
+        image:'../images/iphone11.jpg',
+        price: '₹79,500',
+        brand: 'Apple',
+        rating: 4.5,
+        numReviews: 10
+
+    },
+    {
+        _id:'3',
+        name: 'Realme 7 Pro',
+        image:'../images/realme.jpg',
+        price: '₹31,000',
+        brand: 'Realme',
+        rating: 4.7,
+        numReviews: 23
+
+    },
+    {
+        _id:'4',
+        name: 'Galaxy Note 20',
+        image:'../images/samsung.jpg',
+        price: '₹1,02,500',
+        brand: 'Samsung',
+        rating: 4.8,
+        numReviews: 3
+
+    },
+    {
+        _id:'5',
+        name: 'Mi 10',
+        image:'../images/xiaomi.jpg',
+        price: '₹49,000',
+        brand: 'Xiaomi',
+        rating: 4.1,
+        numReviews: 11
+
+    },
+    {
+        _id:'6',
+        name: 'Xperia XA',
+        image:'../images/xperia.jpg',
+        price: '₹82,000',
+        brand: 'Sony',
+        rating: 4.4,
+        numReviews: 9
+
+    }
+]}
